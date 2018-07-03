b9aad77001535304abbe102b8587214d1b8500b2
kcodek <career_kk@yahoo.com>
2018-07-03 14:32:55 +0530

    ch_04: Database

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,2 +1,9 @@
-from app import app
+from app import app,db
+from app.models import User, Post
+
 #To run the application you set the FLASK_APP=microblog.py in your terminal session, and then execute flask run. 
+#using flask shell, the command pre-imports the application instance.
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User':User, 'Post':Post }
