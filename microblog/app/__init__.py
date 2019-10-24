39ead81cc6abdb48d9df76966ba53e9011e7c72e
kishore.donthikurthi <kishored@ispace.com>
2019-10-24 16:11:27 +0530

    13. I18n and L10n(part_1)

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,7 +9,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +18,7 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 # The Flask-Login extension works with the application's user model,
 # & expects certain properties and methods to be implemented in it
@@ -25,6 +26,8 @@ login = LoginManager(app)
 '''The 'login' value is the function (or endpoint) name for the login view.
 In other words, the name you would use in a url_for() call to get the URL.'''
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -51,6 +54,11 @@ if not app.debug:
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 from app import routes, models, errors
